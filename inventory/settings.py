282e6325b9b97225b864dfb0b48ee80264caa4c1
modoucolley <mbasirucolley@gmail.com>
2022-11-26 12:50:53 +0000

    ssss

--- inventory/settings.py
+++ inventory/settings.py
@@ -82,16 +82,23 @@ WSGI_APPLICATION = 'inventory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'inventory',
-        'USER': 'root',
-        'PASSWORD': '',
-        'PORT': '3306'
+        'HOST': 'sql7.freesqldatabase.com',  
+        'NAME': 'sql7580907',
+        'USER': 'sql7580907',
+        'PASSWORD': 'NPM39Ik9Fi',
+        'PORT': '3306',
+        "OPTIONS": {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+            'charset': 'utf8mb4',
+            "autocommit": True,
+        }
     }
 }
+
+
 """
 DATABASES = {
     'default': {
@@ -99,6 +106,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+"""
 
 
 # Password validation
