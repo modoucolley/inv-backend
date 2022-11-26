d2579d681b8f052eb03c382b779558ab746413e4
modoucolley <mbasirucolley@gmail.com>
2022-11-26 11:21:14 +0000

    ddd

--- inventory/settings.py
+++ inventory/settings.py
@@ -135,8 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = './static/assets/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
