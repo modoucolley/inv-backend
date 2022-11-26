4899a23a59874a5aedc2b83daf8477d980c0ddc5
modoucolley <mbasirucolley@gmail.com>
2022-11-26 13:06:53 +0000

    dddd

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
-web: gunicorn inventory.wsgi --log-file -
+web: gunicorn inventory.wsgi 
+release: python manage.py makemigrations --noinput
+release: python manage.py collectstatic --noinput
+release: python manage.py migrate --noinput
--- inventory/settings.py
+++ inventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k2z9po4i#n+1p(^ny1el2c!om(^-l+_%&ob0azk0-ike*-)81e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'go-inv-backend.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'inventory.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -107,7 +107,7 @@ DATABASES = {
     }
 }
 
-"""
+
 
 
 # Password validation
