4708e1e6fee690fc40de359b0de6270b58ab3b5c
modoucolley <mbasirucolley@gmail.com>
2023-06-14 22:48:16 +0000

    update

--- inventory/urls.py
+++ inventory/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('api/mobile/', include(('mobile.urls', 'mobile'), namespace='mobile')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
- 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+   
