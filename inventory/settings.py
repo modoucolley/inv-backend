717415455bc102d3ff216b8f8834b2ff3cf4465e
modoucolley <mbasirucolley@gmail.com>
2023-06-14 22:24:37 +0000

    update

--- inventory/settings.py
+++ inventory/settings.py
@@ -1,11 +1,15 @@
 
 import os
 
+from decouple import config
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k2z9po4i#n+1p(^ny1el2c!om(^-l+_%&ob0azk0-ike*-)81e'
+SECRET_KEY = config("SECRET_KEY") 
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -21,16 +25,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'whitenoise.runserver_nostatic',
     # local
     'store.apps.StoreConfig',
     'users.apps.UsersConfig',
     'mobile.apps.MobileConfig',
-
     # Third-party apps
     'rest_framework',
     'corsheaders',
     'rest_framework.authtoken'
+    
 ]
 
 MIDDLEWARE = [
@@ -42,7 +46,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'inventory.urls'
