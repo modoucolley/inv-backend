78037789b9ee7da7be4c74c4243f5b7cdc756272
modoucolley <mbasirucolley@gmail.com>
2024-01-21 13:08:19 +0000

    Update on settings File

--- inventory/settings.py
+++ inventory/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/assets/'
 
 STATIC_ROOT = './static/assets/'
 
